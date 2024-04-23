4878649038f0b2bacca4d9e195b6a53ec824eb2e
jose-ribamar <caped.20201p2ads0153@aluno.ifpi.edu.br>
2024-04-23 00:13:42 -0300

    Create excluir_perfil.php
    opção de exclusão perfil candidato

---
+++ backend/candidatos/excluir_perfil.php
@@ -0,0 +1,22 @@
+<?php
+
+include_once('conexao.php');
+$id = $_GET['codigo'];
+
+$sql_eliminar=mysqli_query($ligar, "DELETE FROM tb_usuarios WHERE id='$id' ");
+
+if($sql_eliminar==true){
+    echo "<script>
+        alert('Usuario excluído com sucesso!');
+        window.location.href='../../index.html';
+    </script>";
+
+} else{
+    echo "<script>
+        alert('Falha ao excluir usuário!');
+        window.location.href='../../index.html';
+    </script>";
+}
+
+
+?>
